7dd2f181edd81af4e3997a44c2f284b0bc045871
Anne-Zwift <145778485+Anne-Zwift@users.noreply.github.com>
2025-12-03 13:39:56 +0100

    feat: Configure robust code quality and pre-commit hooks (ESLint, Prettier and Husky).

---
+++ eslint.config.mjs
@@ -0,0 +1,29 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  { 
+    files: ["**/*.{js,mjs,cjs}"], 
+
+    ...js.configs.recommended,
+
+    languageOptions: { 
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        describe: "readonly",
+        test: "readonly",
+        it: "readonly",
+        expect: "readonly",
+      },
+        sourceType: "module",
+    },
+      rules: {
+        "no-console": "warn",
+        "no-unused-vars": "error",
+        "eqeqeq": ["error", "always"],
+
+      }
+  },
+]);
