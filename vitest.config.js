995e3261873d1b38aa432cf6b0fa771b82b753de
Anne-Zwift <145778485+Anne-Zwift@users.noreply.github.com>
2025-12-06 11:55:15 +0100

    chore: Finalize testing setup: Configure Vitest to use JSDOM and disable watch mode for CI (--run)

--- vitest.config.js
+++ vitest.config.js
@@ -3,6 +3,12 @@ import { defineConfig } from 'vite';
 // Simulate the window and location objects
 export default defineConfig({
   test: {
+    include: ['js/tests/**/*.test.js', 'js/tests/**/*/.spec.js'],
+
+    // Exclude root tests like Playwright
+    exclude: ['tests', 'tests/**'],
+
     environment: 'jsdom',
+    globals: true,
   },
 });
