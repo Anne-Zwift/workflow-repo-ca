773fe9602d2575307dbb8e4c4187e25bd2ad1d3d
Anne-Zwift <145778485+Anne-Zwift@users.noreply.github.com>
2025-12-15 13:03:09 +0100

    chore: clean up env example and configuration

--- js/config.js
+++ js/config.js
@@ -1,5 +1,3 @@
 export const CONFIG = {
-  apiUrl:
-    import.meta.env.VITE_API_BASE_URL ||
-    'https://api.noroff.dev/api/v1/holidaze/',
+  apiUrl: 'https://api.noroff.dev/api/v1/holidaze/',
 };
--- playwright.config.js
+++ playwright.config.js
@@ -5,9 +5,10 @@ import { defineConfig, devices } from '@playwright/test';
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
+
 import dotenv from 'dotenv';
-// import path from 'path';
-dotenv.config();
+import path from 'path';
+dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 /**
  * @see https://playwright.dev/docs/test-configuration
@@ -27,7 +28,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: process.env.VITE_API_BASE_URL || 'http://localhost:5173',
+    baseURL: 'http://localhost:5500',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -40,15 +41,15 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewport. */
     // {
@@ -74,7 +75,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run start',
-    url: 'http://localhost:5173',
+    url: 'http://localhost:5500',
     reuseExistingServer: !process.env.CI,
   },
 });
